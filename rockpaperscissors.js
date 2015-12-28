918d4991ad830f8e6da1ed1e7bfc7d0ac7249a04
antonyqparker <tonyramone1990@gmail.com>
2015-12-28 16:33:57 +0000

    final version of RPS.  However, code only logs after all gameplay has been entered rather than displaying each round before promting for another input.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,12 +1,10 @@
-////////////////////////////////////////////////
-/*   Provided Code - Please Don't Edit   */
-////////////////////////////////////////////////
-'use strict';
 
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-    return prompt();
+    console.log();
+    var answer = prompt("Please choose either rock, paper or scissors");
+    return answer;
 }
+
 function randomPlay() {
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
@@ -17,39 +15,71 @@ function randomPlay() {
         return "scissors";
     }
 }
-////////////////////////////////////////////////
-/*           Write Your Code Below            */
-////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return move||getInput();
 }
 
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return move||randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
     var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+ if (playerMove === computerMove){
+        return "tie";
+    } else if (playerMove === "rock" && computerMove === "scissors") {
+        winner = "player";
+    } else if (playerMove === "rock" && computerMove === "paper" ) {
+        winner = "computer";
+    } else if (playerMove === "paper" && computerMove === "scissors") {
+        winner = "computer";
+    } else if (playerMove === "paper" && computerMove === "rock" ) {
+        winner = "player";
+    } else if (playerMove === "scissors" && computerMove === "paper") {
+        winner = "player";
+    } else if (playerMove === "scissors" && computerMove === "rock") {
+        winner = "computer";
+    }
+
     return winner;
 }
 
 function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
-    var playerWins = 0;
-    var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+  console.log('Let\'s play Rock Paper Scissors');
+  var playerWins = 0;
+  var computerWins = 0;
+while (playerWins < 5 && computerWins < 5) {
+    playerMove = getPlayerMove();
+    computerMove = getComputerMove();
+    winner = getWinner(playerMove, computerMove);
+     if (winner === "player") {
+            playerWins += 1;
+            console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+            console.log('player won this round!')
+            console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    }else if (winner === "computer") {
+            computerWins += 1;
+            console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove)
+            console.log('computer won this round!')
+            console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    }else {
+            console.log("Player chose " + playerMove + " while computer chose " + computerMove);
+            console.log("Player and computer have tied this round!");
+            console.log("The score is currently Player: " + playerWins + " to Computer: " + computerWins + ".")
+           }
+            
+    }
+     return "The final score is Player: " + playerWins + " to Computer: " + computerWins + ". Thank your for playing RPS!";
 }
+ 
+playToFive();
+  
+  
+  
+  
+  
+  
+
+
 
